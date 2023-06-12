626eacc0d3e8ed14aebec469e2ee02ec0003cd22
mauromalara <mmalara@outlook.es>
2023-06-12 14:22:46 +0000

    feat(#1)!: add dynamic regex to the generator of callbacks.

--- src/wazuh_testing/utils/callbacks.py
+++ src/wazuh_testing/utils/callbacks.py
@@ -1,31 +1,52 @@
-# Copyright (C) 2015-2023, Wazuh Inc.
-# Created by Wazuh, Inc. <info@wazuh.com>.
-# This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
+"""
+Copyright (C) 2015-2023, Wazuh Inc.
+Created by Wazuh, Inc. <info@wazuh.com>.
+This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
+"""
 import re
 
 from typing import Callable, Tuple, Union
 
 
-def generate_callback(regex: str, keyword: str = None) -> Callable[[str], Union[Tuple[str], None]]:
-    """
-    Returns a callback function that searches for a specified pattern in a given string.
+def replace_values_in_dynamic_regex(regex: str, replacement: dict = None) -> str:
+    """Replace custom values in a regex with "dynamic fields" ("dynamic regex").
 
-    The returned function takes a single argument, `line`, which should be a string.
-    It searches for the pattern specified by the `regex` argument in `line`.
-    If the pattern is found, it will return a tuple containing the whole line that
-    matched the pattern.
-    If the pattern is not found, the function returns `None`.
+    For instance:
+        - A regex with a "dynamic field" look like this: `r'Regex with a dynamic field whose value is {x}'`
+        - A replacement that will override the "dynamic field" with its value look like this: `{'x': 3}`
+
+    The final result is: `r"Regex with a dynamic field whose value is 3"`
+
+    Args:
+        regex (str): A regex pattern with "dynamic fields" to be replaced by real values with `replacement`.
+        replacement (dict): Field-value pair used to make a replacement in the "dynamic regex".
+
+    Returns:
+        regex (str): A regex pattern represented by a string.
+    """
+    if replacement is None:
+        return regex
+
+    for key in replacement:
+        regex = regex.replace(f"{{{key}}}", replacement[key])
+
+    return regex
+
+
+def generate_callback(regex: str, replacement: dict = None) -> Callable:
+    """Returns a callback function that searches for a regular expression in a given line.
 
     Args:
         regex (str): A string representing the regular expression pattern to search for.
+        replacement (dict): Field-value pair used to make a replacement in the regex.
 
     Returns:
-        function: Callback function that takes the line and return the matched substring
-                  if found, or `None` otherwise.
+        new_callback (Callable): Searches for a match in a string given an specific regex.
     """
+    regex = replace_values_in_dynamic_regex(regex, replacement)
+
     def new_callback(line: str) -> Union[Tuple[str], None]:
-        """
-        Callback function that looks for the specified regular expression pattern in a string.
+        """Callback function that looks for the specified regular expression pattern in a string.
 
         Args:
             line (str): The string to search for a match.
@@ -34,11 +55,8 @@ def generate_callback(regex: str, keyword: str = None) -> Callable[[str], Union[
             tuple[str] | None: A tuple containing the matched substring if found, otherwise `None`.
         """
         # Match the received line.
-        if keyword is None or keyword in line:
-            match = re.match(regex, line)
-            # Return the matched string.
-            return match.groups() if match else None
-        else:
-            return None
+        match = re.match(regex, line)
+        # Return the matched string.
+        return match.groups() if match else None
 
     return new_callback
