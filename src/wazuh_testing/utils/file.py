8079070a91a37cf45a1abd97a0ab2a4cd551fe11
Deblintrake09 <amicalizzi2005@gmail.com>
2023-06-13 10:30:09 -0300

    Add new file management functions

--- src/wazuh_testing/utils/file.py
+++ src/wazuh_testing/utils/file.py
@@ -3,6 +3,9 @@
 # This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
 import chardet
 import os
+import sys
+import shutil
+import stat
 import json
 import yaml
 
@@ -68,6 +71,52 @@ def copy(source, destination):
 
     if sys.platform != 'win32':
         os.chown(destination, source_stats[stat.ST_UID], source_stats[stat.ST_GID])
+
+
+def remove_file(file_path):
+    """Remove a file or a directory path.
+
+    Args:
+        file_path (str): File or directory path to remove.
+    """
+    if os.path.exists(file_path):
+        if os.path.isfile(file_path):
+            os.remove(file_path)
+        elif os.path.isdir(file_path):
+            delete_path_recursively(file_path)
+
+
+def delete_path_recursively(path):
+    '''Remove a directory recursively.
+
+    Args:
+        path (str): Directory path.
+    '''
+    if os.path.exists(path):
+        shutil.rmtree(path, onerror=on_write_error)
+
+
+def on_write_error(function, path, exc_info):
+    """ Error handler for functions that try to modify a file. If the error is due to an access error (read only file),
+    it attempts to add write permission and then retries. If the error is for another reason it re-raises the error.
+
+    Args:
+        function (function): function that called the handler.
+        path (str): Path to the file the function is trying to modify
+        exc_info (object): function instance execution information. Passed in by function in runtime.
+
+    Example:
+        > shutil.rmtree(path, onerror=on_write_error)
+    """
+    import stat
+    # Check if the error is an access error for Write permissions.
+    if not os.access(path, os.W_OK):
+        # Add write permissions so file can be edited and execute function.
+        os.chmod(path, 0o0777)
+        function(path)
+    # If error is not Write access error, raise the error
+    else:
+        raise
 
 
 def read_yaml(file_path):
