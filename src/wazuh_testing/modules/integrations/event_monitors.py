b16766e6460f219e034d0dd1fc3768ded4e64291
Manuel <manuel.cano@wazuh.com>
2023-06-19 17:05:22 +0200

    Cleaned old callback functions

--- src/wazuh_testing/modules/integrations/event_monitors.py
+++ src/wazuh_testing/modules/integrations/event_monitors.py
@@ -13,9 +13,9 @@ def detect_wrong_content_config(error_type, tag, integration, file_monitor):
         integration (str): Integration name
         file_monitor (FileMonitor): File log monitor to detect events
     """   
-    callback = f".*ERROR: '{error_type}' content for tag '{tag}' at module '{integration}'."
+    callback = f".*ERROR: {error_type} content for tag '{tag}' at module '{integration}'."
     
-    return file_monitor.start(timeout=10, callback=generate_callback(callback))
+    return file_monitor.start(timeout=180, callback=generate_callback(callback))
 
 def detect_integration_start(integration, file_monitor):
     """Detect module GitHub starts after restarting Wazuh.
@@ -27,67 +27,3 @@ def detect_integration_start(integration, file_monitor):
     callback = f".*INFO: Module {integration} started."
 
     file_monitor.start(timeout=60, callback=generate_callback(callback))
-
-
-
-
-
-def callback_detect_enabled_err(line):
-    if f"ERROR: Invalid content for tag 'enabled' at module 'GitHub'." in line:
-        return line
-    return None
-
-
-def callback_detect_only_future_events_err(integration,line):
-    if f"ERROR: Invalid content for tag 'only_future_events' at module 'GitHub'." in line:
-        return line
-    return None
-
-
-def callback_detect_interval_err(integration, line):
-    if f"ERROR: Invalid content for tag 'interval' at module 'GitHub'." in line:
-        return line
-    return None
-
-
-def callback_detect_curl_max_size_err(integration, line):
-    if f"ERROR: Invalid content for tag 'curl_max_size' at module 'GitHub'. The minimum value allowed is 1KB." in line:
-        return line
-    return None
-
-
-def callback_detect_time_delay_err(integration, line):
-    if f"ERROR: Invalid content for tag 'time_delay' at module 'GitHub'." in line:
-        return line
-    return None
-
-
-def callback_detect_org_name_err(integration, line):
-    if f'ERROR: Empty content for tag \'org_name\' at module \'GitHub\'.' in line:
-        return line
-    return None
-
-
-def callback_detect_api_token_err(integration, line):
-    if f'ERROR: Empty content for tag \'api_token\' at module \'GitHub\'.' in line:
-        return line
-    return None
-
-
-def callback_detect_event_type_err(integration, line):
-    if f'ERROR: Invalid content for tag \'event_type\' at module \'GitHub\'.' in line:
-        return line
-    return None
-
-
-def callback_detect_read_err(integration, line):
-    if f'ERROR: Empty content for tag \'api_auth\' at module \'GitHub\'.' in line:
-        return line
-    return None
-
-
-def callback_detect_github_start(integration, line):
-    if f'INFO: Module GitHub started.' in line:
-        return line
-    return None
-
