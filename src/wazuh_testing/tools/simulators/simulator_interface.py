5d450ef9e855871dad11c9e1b1aadf9fa6e64094
quebim <kevin.ledesma@wazuh.com>
2023-06-22 11:05:58 -0300

    docs(#2): add docstrings to SimulatorInterface

--- src/wazuh_testing/tools/simulators/simulator_interface.py
+++ src/wazuh_testing/tools/simulators/simulator_interface.py
@@ -2,9 +2,31 @@ from abc import ABC, abstractmethod
 
 
 class SimulatorInterface(ABC):
+    """
+    An abstract base class that defines the interface for a simulator.
+
+    A simulator is a class that can simulate the behavior of a Wazuh component
+    by sending and receiving messages through a socket.
+
+    Attributes:
+        MODES (list): A list of valid modes for the simulator.
+        server_ip (str): The IP address of the Wazuh server.
+        port (int): The port number of the Wazuh server.
+        running (bool): A flag that indicates if the simulator is running or not.
+        mode (str): The current mode of the simulator. Must be one of MODES.
+    """
+
     MODES: list
 
     def __init__(self, server_ip: str, port: int, running: bool) -> None:
+        """
+        Initialize a SimulatorInterface object.
+
+        Args:
+            server_ip (str): The IP address of the Wazuh server.
+            port (int): The port number of the Wazuh server.
+            running (bool): A flag that indicates if the simulator is running or not.
+        """
         self.server_ip = server_ip
         self.port = port
         self.running = running
@@ -12,24 +34,20 @@ class SimulatorInterface(ABC):
     @property
     def mode(self):
         """
-        Get the mode of operation for the simulator.
+        Get the mode of the simulator.
 
         Returns:
-            str: The current mode of operation.
+            str: The current mode of the simulator.
         """
         return self.__mode
 
     @mode.setter
     def mode(self, mode: str) -> None:
         """
-        Set the mode of operation for the simulator.
-        It must be a valid mode from the list MODES.
-
-        Args:
-            mode str): The mode of operation to set.
+        Set the mode of the simulator.
 
         Raises:
-            ValueError: If the specified mode is not valid.
+            ValueError: If the mode is not one of MODES.
         """
         if mode.upper() not in self.MODES:
             raise ValueError(f'Invalid mode. Modes: {self.MODES}')
@@ -38,8 +56,18 @@ class SimulatorInterface(ABC):
 
     @abstractmethod
     def start():
+        """
+        Start the simulator.
+
+        This method should be implemented by subclasses to start the simulation process.
+        """
         pass
 
     @abstractmethod
     def shutdown():
+        """
+        Shutdown the simulator.
+
+        This method should be implemented by subclasses to stop the simulation process.
+        """
         pass
