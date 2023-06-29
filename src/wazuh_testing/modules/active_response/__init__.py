0c1e900db37eba5731d579eeabd87cefeb1b596f
quebim <kevin.ledesma@wazuh.com>
2023-06-28 21:04:20 -0300

    feat(#2): fix error on file.py util and add ar binary path

--- src/wazuh_testing/modules/active_response/__init__.py
+++ src/wazuh_testing/modules/active_response/__init__.py
@@ -1,10 +1,3 @@
 # Copyright (C) 2015-2021, Wazuh Inc.
 # Created by Wazuh, Inc. <info@wazuh.com>.
 # This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
-import os
-
-from wazuh_testing.constants.paths import WAZUH_PATH
-
-# Active response binaries path
-ACTIVE_RESPONSE_BINARIES = os.path.join(WAZUH_PATH, 'active-response', 'bin')
-FIREWALL_DROP_BIN = os.path.join(ACTIVE_RESPONSE_BINARIES, 'firewall-drop')
