36215d9581f9c8160afb9a1a7ee75519d2a99d68
quebim <kevin.ledesma@wazuh.com>
2023-07-12 11:13:27 -0300

    feat(#20): add first FIM patterns and update debug options

--- src/wazuh_testing/utils/file.py
+++ src/wazuh_testing/utils/file.py
@@ -223,3 +223,13 @@ def exists_and_is_file(path: str) -> bool:
         bool: True if the file exists and is a regular file, False otherwise.
     """
     return os.path.exists(path) and os.path.isfile(path)
+
+
+def create_folder(path: str) -> None:
+    if not os.path.exists(path):
+        os.mkdir(path, mode=0o0777)
+
+
+def remove_folder(path: str) -> None:
+    if os.path.exists(path):
+        shutil.rmtree(path, onerror=on_write_error)
