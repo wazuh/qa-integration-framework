e30dec121d1022db3203bd00c0566acab9970419
Tomás Turina <tomas.turina@wazuh.com>
2023-07-21 11:23:52 -0300

    Remove extra whitespace in patterns modules

--- src/wazuh_testing/modules/modulesd/patterns.py
+++ src/wazuh_testing/modules/modulesd/patterns.py
@@ -3,7 +3,6 @@
 # This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
 
 
-
 # Callback patterns to find events in log file.
 MODULESD_CONFIGURATION_ERROR = r".*ERROR: {error_type} content for tag '{tag}' at module '{integration}'."
 MODULESD_STARTED = r".*INFO: Module {integration} started."
