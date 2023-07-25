3f5c32c968b4c9211e320d295a22eb3034ac5132
quebim <kevin.ledesma@wazuh.com>
2023-07-25 19:22:37 -0300

    feat(#20): add function to delete all files inside a folder

--- src/wazuh_testing/utils/file.py
+++ src/wazuh_testing/utils/file.py
@@ -486,10 +486,37 @@ def get_list_of_content_yml(file_path, separator='_'):
 
 
 def create_folder(path: str) -> None:
+    """Create a folder.
+
+    Args:
+        path (str): Path of the folder to be created.
+    """
     if not os.path.exists(path):
         os.mkdir(path, mode=0o0777)
 
 
 def remove_folder(path: str) -> None:
+    """Remove a folder.
+
+    Args:
+        path (str): Path of the folder to be removed.
+    """
     if os.path.exists(path):
         shutil.rmtree(path, onerror=on_write_error)
+
+
+def delete_files_in_folder(folder_path):
+    """Delete all files in a folder.
+
+    Args:
+        folder_path (str): Path of the folder containing the files to be deleted.
+    """
+    for filename in os.listdir(folder_path):
+        file_path = os.path.join(folder_path, filename)
+        try:
+            if os.path.isfile(file_path) or os.path.islink(file_path):
+                os.unlink(file_path)
+            elif os.path.isdir(file_path):
+                shutil.rmtree(file_path)
+        except Exception as e:
+            print(f'Failed to delete {file_path}. Reason: {e}')
