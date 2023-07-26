3361dadb1a4b22bd091a58b9e6eaf64bcc049050
quebim <kevin.ledesma@wazuh.com>
2023-07-26 14:32:30 -0300

    fix(#20): fix REALTIME_WHODATA pattern

--- src/wazuh_testing/modules/fim/patterns.py
+++ src/wazuh_testing/modules/fim/patterns.py
@@ -5,9 +5,11 @@
 # Callback patterns to find events in log file.
 IGNORING_DUE_TO_SREGEX = r".*?Ignoring path '(.*)' due to sregex '(.*)'.*"
 IGNORING_DUE_TO_PATTERN = r".*?Ignoring path '(.*)' due to pattern '(.*)'.*"
-REALTIME_WHODATA_ENGINE_STARTED = r'.*File integrity monitoring real-time Whodata engine started.'
+REALTIME_WHODATA_ENGINE_STARTED = r'.*File integrity monitoring real-time Whodata engine started.*'
 MONITORING_PATH = r'.*Monitoring path:.*'
 SENDING_FIM_EVENT =  r'.*Sending FIM event:.*'
 WHODATA_ADDED_EVENT = fr"{SENDING_FIM_EVENT}added.*"
 WHODATA_DELETED_EVENT = fr"{SENDING_FIM_EVENT}deleted.*"
 WHODATA_NOT_STARTED = r'.*Who-data engine could not start. Switching who-data to real-time.'
+
+GET_FIM_EVENT_JSON =  r'.*Sending FIM event: (.+)$'
