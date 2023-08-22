40c5ecba953e63a7a52e6d798c19d2a435eecdb0
quebim <kevin.ledesma@wazuh.com>
2023-08-22 15:37:31 -0300

    feat(#20): add new pattern to FIM module

--- src/wazuh_testing/modules/fim/patterns.py
+++ src/wazuh_testing/modules/fim/patterns.py
@@ -13,5 +13,6 @@ DELETED_EVENT = r'.*"type":"deleted".*'
 INODE_ENTRIES_PATH_COUNT =  r".*Fim inode entries: '(\d+)', path count: '(\d+)'"
 WHODATA_NOT_STARTED = r'.*Who-data engine could not start. Switching who-data to real-time.'
 EMPTY_DIRECTORIES_TAG = r'.*Empty directories tag found in the configuration.*'
+FILE_LIMIT_PERCENTAGE = r'.*File database is (\d+)% full.'
 
 EXTRACT_EVENT_JSON =  r'.*Sending FIM event: (.+)$'
