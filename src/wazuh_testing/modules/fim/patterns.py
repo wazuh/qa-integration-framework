49a5180de5a5feabb1315d615d799d7af559d8fc
quebim <kevin.ledesma@wazuh.com>
2023-08-24 10:54:56 -0300

    feat(#20): improve patterns naming | move files from syscheck module to FIM | add commands utils

--- src/wazuh_testing/modules/fim/patterns.py
+++ src/wazuh_testing/modules/fim/patterns.py
@@ -7,10 +7,14 @@ IGNORING_DUE_TO_SREGEX = r".*?Ignoring path '(.*)' due to sregex '(.*)'.*"
 IGNORING_DUE_TO_PATTERN = r".*?Ignoring path '(.*)' due to pattern '(.*)'.*"
 REALTIME_WHODATA_ENGINE_STARTED = r'.*File integrity monitoring real-time Whodata engine started.*'
 MONITORING_PATH = r'.*Monitoring path:.*'
-SENDING_FIM_EVENT =  r'.*Sending FIM event: .*"type":"event".*'
-ADDED_EVENT = r'.*"type":"added".*'
-DELETED_EVENT = r'.*"type":"deleted".*'
-INODE_ENTRIES_PATH_COUNT =  r".*Fim inode entries: '(\d+)', path count: '(\d+)'"
+
+# Events
+SENDING_FIM_EVENT = r'.*Sending FIM event: .*"type":"event".*'
+EVENT_TYPE_ADDED = r'.*"type":"added".*'
+EVENT_TYPE_DELETED = r'.*"type":"deleted".*'
+FIM_EVENT_JSON = r'.*Sending FIM event: (.+)$'
+
+INODE_ENTRIES_PATH_COUNT = r".*Fim inode entries: '(\d+)', path count: '(\d+)'"
 WHODATA_NOT_STARTED = r'.*Who-data engine could not start. Switching who-data to real-time.'
 EMPTY_DIRECTORIES_TAG = r'.*Empty directories tag found in the configuration.*'
 
@@ -18,4 +22,5 @@ FILE_LIMIT_PERCENTAGE = r'.*File database is (\d+)% full.'
 FILE_LIMIT_DISABLED = r'.*No limit set to maximum number of file entries to be monitored'
 FILE_LIMIT_AMOUNT = r".*Maximum number of files to be monitored: '(\d+)'"
 
-EXTRACT_EVENT_JSON =  r'.*Sending FIM event: (.+)$'
+LINKS_SCAN_FINALIZED = r'.*Links check finalized.*'
+AUDIT_RULES_RELOADED = r'.*Audit rules reloaded\. Rules loaded: (.+)'
