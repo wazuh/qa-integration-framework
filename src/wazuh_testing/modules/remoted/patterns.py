e16ce873a733a8385b8c61f8c4f6a48938153a3a
thejbte <thejbte@gmail.com>
2023-10-05 18:36:43 +0000

    added remoted patterns

--- src/wazuh_testing/modules/remoted/patterns.py
+++ src/wazuh_testing/modules/remoted/patterns.py
@@ -2,3 +2,6 @@ from . import PREFIX
 
 INVALID_VALUE_FOR_ELEMENT = fr"{PREFIX}.*Invalid value for element.*"
 CONFIGURATION_ERROR = r".*{severity}:.*Configuration error at '{path}'.*"
+
+INVALID_VALUE_FOR_PORT_NUMBER = fr"{PREFIX}.*Invalid port number.*"
+CONFIGURATION_ERROR_PORT = r".*{severity}: \\(\\d+\\): Invalid port number: '{port}'.*"
