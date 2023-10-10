0c90b205bf089b0033391e20d92714025066e33f
thejbte <thejbte@gmail.com>
2023-10-10 20:45:21 +0000

    added remoted patterns: invalid connection protocol

--- src/wazuh_testing/modules/remoted/patterns.py
+++ src/wazuh_testing/modules/remoted/patterns.py
@@ -5,3 +5,7 @@ CONFIGURATION_ERROR = r".*{severity}:.*Configuration error at '{path}'.*"
 
 INVALID_VALUE_FOR_PORT_NUMBER = fr"{PREFIX}.*Invalid port number.*"
 CONFIGURATION_ERROR_PORT = r".*{severity}: \\(\\d+\\): Invalid port number: '{port}'.*"
+
+IGNORED_INVALID_PROTOCOL = r".*WARNING:.* Ignored invalid value '{protocol}' for 'protocol'.*"
+ERROR_GETTING_PROTOCOL = fr"{PREFIX}.* Error getting protocol. Default value \(TCP\) will be used.*"
+DETECT_REMOTED_STARTED = r".*Started.*Listening on port {port}\/{protocol_valid_upper} \({connection}\).*"
