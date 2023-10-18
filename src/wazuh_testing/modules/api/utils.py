50ebfb271869ab0198a73dcde2ad8b55f5b6bf5d
thejbte <thejbte@gmail.com>
2023-10-18 18:25:41 +0000

    added API methods to remoted

--- src/wazuh_testing/modules/api/utils.py
+++ src/wazuh_testing/modules/api/utils.py
@@ -297,3 +297,115 @@ def validate_statistics(response: requests.Response, schema_path: Union[str, os.
     """
     stats_schema = read_json_file(schema_path)
     validate(instance=response.json(), schema=stats_schema)
+
+
+def compare_config_api_response(configuration, section):
+    """Assert if configuration values provided are the same that configuration provided for API response.
+
+    Args:
+        configuration (dict): Dictionary with Wazuh manager configuration.
+        section (str): Section to compare.
+    """
+    import sys
+    from sys import stderr
+
+    api_answer = get_manager_configuration(section=section)
+    assert type(api_answer) == type(configuration)
+
+    if isinstance(api_answer, list):
+        configuration_length = len(configuration)
+        for i in range(configuration_length):
+            api_answer_to_compare = dict((key, api_answer[i][key]) for key in configuration[i].keys())
+            configuration_to_compare = dict((key, configuration[i][key]['value']) for key in configuration[i].keys())
+            assert api_answer_to_compare == configuration_to_compare
+    else:
+        api_answer_to_compare = dict((key, api_answer[key]) for key in configuration.keys())
+        assert api_answer_to_compare == configuration
+
+
+def get_manager_configuration(section=None, field=None):
+    """Get Wazuh manager configuration response from API using GET /manager/configuration
+
+    References: https://documentation.wazuh.com/current/user-manual/api/reference.html#operation/
+                api.controllers.manager_controller.get_configuration
+
+    Args:
+        section (str): wazuh configuration section, E.g: "active-response", "ruleset"...
+        field   (str): section child. E.g, fields for ruleset section are: decoder_dir, rule_dir, etc
+
+    Returns:
+        `obj`(str or map): active configuration indicated by Wazuh API. If section and field are selected, it will
+         return a String, if not, it will return a map for the section/entire configurations with fields/sections
+         as keys.
+    """
+    api_details = get_api_details_dict()
+    api_query = f"{api_details['base_url']}/manager/configuration?"
+
+    if section is not None:
+        api_query += f"section={section}"
+        if field is not None:
+            api_query += f"&field={field}"
+
+    response = requests.get(api_query, headers=api_details['auth_headers'], verify=False)
+
+    assert response.json()['error'] == 0, f"Wazuh API response status different from 0: {response.json()}"
+    answer = response.json()['data']['affected_items'][0]
+
+    def get_requested_values(answer, section, field):
+        """Return requested value from API response
+
+        Received a section and a field and tries to return all available values that match with this entry.
+        This function is required because, sometimes, there may be multiple entries with the same field or section
+        and the API will return a list instead of a map. Using recursion we make sure that the output matches
+        the user expectations.
+        """
+        if isinstance(answer, list):
+            new_answer = []
+            for element in answer:
+                new_answer.append(get_requested_values(element, section, field))
+            return new_answer
+        elif isinstance(answer, dict):
+            if section in answer.keys():
+                new_answer = answer[section]
+                return get_requested_values(new_answer, section, field)
+            if field in answer.keys():
+                new_answer = answer[field]
+                return get_requested_values(new_answer, section, field)
+        return answer
+
+    return get_requested_values(answer, section, field)
+
+
+def get_api_details_dict(protocol=WAZUH_API_PROTOCOL, host=WAZUH_API_HOST, port=WAZUH_API_PORT, user=WAZUH_API_USER, password=WAZUH_API_PASSWORD,
+                         login_endpoint=LOGIN_ROUTE, timeout=10, login_attempts=1, sleep_time=0):
+    """Get API details"""
+    login_token = get_token_login_api(protocol, host, port, user, password, login_endpoint, timeout, login_attempts,
+                                      sleep_time)
+    return {
+        'base_url': get_base_url(protocol, host, port),
+        'auth_headers': {
+            'Content-Type': 'application/json',
+            'Authorization': f'Bearer {login_token}'
+        }
+    }
+
+
+def get_token_login_api(protocol, host, port, user, password, login_endpoint, timeout, login_attempts, sleep_time):
+    """Get API login token"""
+
+    login_url = f"{get_base_url(protocol, host, port)}{login_endpoint}"
+
+    for _ in range(login_attempts):
+        response = requests.post(login_url, headers=get_login_headers(user, password), verify=False, timeout=timeout)
+
+        if response.status_code == 200:
+            return json.loads(response.content.decode())['data']['token']
+        time.sleep(sleep_time)
+    else:
+        raise RuntimeError(f"Error obtaining login token: {response.json()}")
+
+
+def get_login_headers(user, password):
+    basic_auth = f"{user}:{password}".encode()
+    return {'Content-Type': 'application/json',
+            'Authorization': f'Basic {b64encode(basic_auth).decode()}'}
