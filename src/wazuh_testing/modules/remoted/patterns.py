828374c4822fdd7cf02c18193c296016607828b3
thejbte <thejbte@gmail.com>
2023-10-19 19:33:07 +0000

    added remoted patterns: queue size syslog

--- src/wazuh_testing/modules/remoted/patterns.py
+++ src/wazuh_testing/modules/remoted/patterns.py
@@ -14,3 +14,5 @@ WARNING_SYSLOG_TCP_UDP = r".*WARNING:.*Only secure connection supports TCP and U
                          r"Default value \(TCP\) will be used.*"
 
 ERROR_BIND_PORT = r".*CRITICAL: \(\d+\): Unable to Bind port '1514' due to \[\(\d+\)\-\(Cannot assign requested address\)\]"
+
+ERROR_QUEUE_SIZE_SYSLOG = r".*ERROR: Invalid option \<queue_size\> for Syslog remote connection."
