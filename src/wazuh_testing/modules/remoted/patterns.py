58ade14f70dfc9d70ffc7011d76890853cc5a276
thejbte <thejbte@gmail.com>
2023-10-19 18:08:00 +0000

    added remoted patterns: invalid local ip

--- src/wazuh_testing/modules/remoted/patterns.py
+++ src/wazuh_testing/modules/remoted/patterns.py
@@ -12,3 +12,5 @@ DETECT_REMOTED_STARTED = r".*Started.*Listening on port {port}\/{protocol_valid_
 
 WARNING_SYSLOG_TCP_UDP = r".*WARNING:.*Only secure connection supports TCP and UDP at the same time. " \
                          r"Default value \(TCP\) will be used.*"
+
+ERROR_BIND_PORT = r".*CRITICAL: \(\d+\): Unable to Bind port '1514' due to \[\(\d+\)\-\(Cannot assign requested address\)\]"
