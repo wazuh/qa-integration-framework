d26298c53e1b7f13627c8789a1438f29c766da00
Eduardo <eduardo.leon@wazuh.com>
2023-11-21 14:22:57 -0300

    Add missing imports, remove unused methods, add new pattern

--- src/wazuh_testing/constants/aws.py
+++ src/wazuh_testing/constants/aws.py
@@ -2,9 +2,10 @@
 AWS_LOGS = 'AWSLogs'
 AWS_MODULE_CALL = "Calling AWS module with: '%s'"
 
-RANDOM_ACCOUNT_ID = '819751203817'
+RANDOM_ACCOUNT_ID = '819751203818'
 VPC_FLOW_LOGS = 'vpcflowlogs'
-FLOW_LOG_ID = 'fl-0755d951c16f517fa'
+FLOW_LOG_ID = 'fl-0754d951c16f517fa'
+GUARDDUTY = 'GuardDuty'
 CONFIG = 'Config'
 ELASTIC_LOAD_BALANCING = 'elasticloadbalancing'
 SERVER_ACCESS_TABLE_NAME = 's3_server_access'
@@ -33,6 +34,9 @@ JSON_GZ_EXT = '.jsonl.gz'
 CSV_EXT = '.csv'
 
 # Bucket types
+CLOUDTRAIL = 'CloudTrail'
+GUARDDUTY = 'GuardDuty'
+VPC_FLOW_LOGS = 'vpcflowlogs'
 CLOUD_TRAIL_TYPE = 'cloudtrail'
 VPC_FLOW_TYPE = 'vpcflow'
 CONFIG_TYPE = 'config'
@@ -55,25 +59,3 @@ INSPECTOR_TYPE = 'inspector'
 
 # Params
 ONLY_LOGS_AFTER_PARAM = '--only_logs_after'
-
-# Databases
-SELECT_QUERY_TEMPLATE = 'SELECT * FROM {table_name}'
-
-# Errors
-FAILED_START = "The AWS module did not start as expected"
-INCORRECT_PARAMETERS = "The AWS module was not called with the correct parameters"
-ERROR_FOUND = "Found error message on AWS module"
-INCORRECT_EVENT_NUMBER = "The AWS module did not process the expected number of events"
-INCORRECT_NON_EXISTENT_REGION_MESSAGE = "The AWS module did not show correct message about non-existent region"
-INCORRECT_NO_EXISTENT_LOG_GROUP = "The AWS module did not show correct message non-existent log group"
-INCORRECT_EMPTY_PATH_MESSAGE = "The AWS module did not show correct message about empty path"
-INCORRECT_EMPTY_PATH_SUFFIX_MESSAGE = "The AWS module did not show correct message about empty path_suffix"
-INCORRECT_ERROR_MESSAGE = "The AWS module did not show the expected error message"
-INCORRECT_EMPTY_VALUE_MESSAGE = "The AWS module did not show the expected message about empty value"
-INCORRECT_LEGACY_WARNING = "The AWS module did not show the expected legacy warning"
-INCORRECT_WARNING = "The AWS module did not show the expected warning"
-INCORRECT_INVALID_VALUE_MESSAGE = "The AWS module did not show the expected message about invalid value"
-INCORRECT_MARKER = "The AWS module did not use the correct marker"
-INCORRECT_SERVICE_CALLS_AMOUNT = "The AWS module was not called for bucket or service the right amount of times"
-UNEXPECTED_NUMBER_OF_EVENTS_FOUND = "Some logs may were processed or the results found are more than expected"
-POSSIBLY_PROCESSED_LOGS = "Some logs may were processed"
