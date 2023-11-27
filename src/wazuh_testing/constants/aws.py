38f0f0dc821387fb284fe2149ec067d43aa1d392
Eduardo <eduardo.leon@wazuh.com>
2023-11-27 19:37:08 -0300

    Add copyright header to files

--- src/wazuh_testing/constants/aws.py
+++ src/wazuh_testing/constants/aws.py
@@ -1,3 +1,11 @@
+# Copyright (C) 2015, Wazuh Inc.
+# Created by Wazuh, Inc. <info@wazuh.com>.
+# This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
+
+"""
+    File contains all constants used in the AWS test suite.
+"""
+
 # AWS Constants
 AWS_LOGS = 'AWSLogs'
 AWS_MODULE_CALL = "Calling AWS module with: '%s'"
--- src/wazuh_testing/constants/paths/aws.py
+++ src/wazuh_testing/constants/paths/aws.py
@@ -1,3 +1,11 @@
+# Copyright (C) 2015, Wazuh Inc.
+# Created by Wazuh, Inc. <info@wazuh.com>.
+# This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
+
+"""
+    File contains all paths variables used in the AWS test suite.
+"""
+
 from pathlib import Path
 
 # Local imports
