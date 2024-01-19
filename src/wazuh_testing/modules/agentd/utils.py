662ebded97e214eaff4b7ac06383ea52b1827bc0
Manuel <manuel.cano@wazuh.com>
2024-01-19 10:44:21 +0100

    Added parse_state_file function to framework

---
+++ src/wazuh_testing/modules/agentd/utils.py
@@ -0,0 +1,25 @@
+# Copyright (C) 2015-2023, Wazuh Inc.
+# Created by Wazuh, Inc. <info@wazuh.com>.
+# This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
+
+from wazuh_testing.constants.paths.variables import AGENTD_STATE
+
+
+def parse_state_file():
+    """Parse state file
+
+    Returns:
+        state info
+    """
+    state = {}
+    with open(AGENTD_STATE) as state_file:
+        for line in state_file:
+            line = line.rstrip('\n')
+            # Remove empty lines or comments
+            if not line or line.startswith('#'):
+                continue
+            (key, value) = line.split('=', 1)
+            # Remove value's quotes
+            state[key] = value.strip("'")
+
+    return state
