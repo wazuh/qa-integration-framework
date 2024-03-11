760072e0d914c1f15eb7c778c19dd0fc67bcea9a
Tomas Turina <tomas.turina@wazuh.com>
2024-03-11 21:10:11 +0000

    Add new patterns to integratord mmodule

--- src/wazuh_testing/modules/integratord/patterns.py
+++ src/wazuh_testing/modules/integratord/patterns.py
@@ -10,3 +10,7 @@ INTEGRATORD_THIRD_PARTY_RESPONSE = r'.*<Response \[.*\]>'
 INTEGRATORD_INODE_CHANGED = r'.*DEBUG: jqueue_next.*Alert file inode changed.*'
 INTEGRATORD_INVALID_ALERT_READ = r'.*WARNING: Invalid JSON alert read.*'
 INTEGRATORD_OVERLONG_ALERT_READ = r'.*WARNING: Overlong JSON alert read.*'
+INTEGRATORD_ENABLED_INTEGRATION = r".*(Enabling integration for: '{integration}')."
+INTEGRATORD_OPTIONS_FILE_DOES_NOT_EXISTENT = ".*OS_IntegratorD.*(JSON file for options  doesn't exist)"
+INTEGRATORD_SENDING_MESSAGE = r'.*Sending message (.*) to {integration} server'
+INTEGRATORD_ERROR_RUNNING_INTEGRATION = r".*ERROR: Unable to run integration for ({integration}) -> integrations"
