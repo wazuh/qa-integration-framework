fc4126a9ed005f36205370767725fbf5d57dcad0
Tomas Turina <tomas.turina@wazuh.com>
2024-03-27 18:41:29 +0000

    Add some enrollment patterns

--- src/wazuh_testing/modules/remoted/patterns.py
+++ src/wazuh_testing/modules/remoted/patterns.py
@@ -2,7 +2,6 @@
 # Created by Wazuh, Inc. <info@wazuh.com>.
 # This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
 
-
 # Callback patterns to find events in log file.
 from . import PREFIX
 
