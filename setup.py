25eb99a8743b26d01b12192ae2660bb845990112
Selutario <joseluis.lopez@wazuh.com>
2024-05-15 12:08:58 +0200

    Bump to 4.7.5, update changelog

--- setup.py
+++ setup.py
@@ -60,7 +60,7 @@ def get_install_requires() -> List[str]:
 
 setup(
     name='wazuh_testing',
-    version='4.7.4',
+    version='4.7.5',
     description='Wazuh testing utilities to help programmers automate tests',
     url='https://github.com/wazuh',
     author='Wazuh',
