eefa745a800d8768bba546d0b6981c1d7afc80e6
Santiago Vendramini <sdvendramini@gmail.com>
2024-05-20 17:30:14 -0300

    Patterns and functions to run test to restrict registry keys monitoring added.

--- src/wazuh_testing/modules/fim/patterns.py
+++ src/wazuh_testing/modules/fim/patterns.py
@@ -5,6 +5,7 @@
 # Callback patterns to find events in log file.
 IGNORING_DUE_TO_SREGEX = r".*?Ignoring path '(.*)' due to sregex '(.*)'.*"
 IGNORING_DUE_TO_PATTERN = r".*?Ignoring path '(.*)' due to pattern '(.*)'.*"
+IGNORING_DUE_TO_RESTRICTION = r".*Ignoring entry '(.*?)' due to restriction '.*?'"
 REALTIME_WHODATA_ENGINE_STARTED = r'.*File integrity monitoring real-time Whodata engine started.*'
 MONITORING_PATH = r'.*Monitoring path:.*'
 
--- src/wazuh_testing/modules/fim/utils.py
+++ src/wazuh_testing/modules/fim/utils.py
@@ -4,6 +4,9 @@
 
 import json
 import re
+import win32con
+import win32api
+import pywintypes
 
 from .patterns import FIM_EVENT_JSON
 
@@ -20,3 +23,42 @@ def get_fim_event_data(message: str) -> dict:
     """
     to_json = re.match(FIM_EVENT_JSON, message)
     return json.loads(to_json.group(1)).get('data')
+
+
+def delete_registry(key, sub_key, arch):
+    """
+    Delete a registry key.
+
+    Args:
+        key (pyHKEY): the key of the registry (HKEY_* constants).
+        subkey (str): the subkey (name) of the registry.
+        arch (int): architecture of the registry (KEY_WOW64_32KEY or KEY_WOW64_64KEY).
+    """
+
+    try:
+        key_h = win32api.RegOpenKeyEx(key, sub_key, 0, win32con.KEY_ALL_ACCESS | arch)
+        win32api.RegDeleteTree(key_h, None)
+        win32api.RegDeleteKeyEx(key, sub_key, samDesired=arch)
+    except OSError as e:
+        print(f"Couldn't remove registry key: {e}")
+    except pywintypes.error as e:
+        print(f"Couldn't remove registry key: {e}")
+
+
+def create_registry(key, sub_key, arch):
+    """
+    Create a registry given the key and the subkey.
+
+    Args:
+        key (pyHKEY): the key of the registry (HKEY_* constants).
+        subkey (str): the subkey (name) of the registry.
+        arch (int): architecture of the registry (KEY_WOW64_32KEY or KEY_WOW64_64KEY).
+    """
+
+    try:
+        win32api.RegCreateKeyEx(key, sub_key, win32con.KEY_ALL_ACCESS | arch)
+
+    except OSError as e:
+        print(f"Registry could not be created: {e}")
+    except pywintypes.error as e:
+        print(f"Registry could not be created: {e}")
