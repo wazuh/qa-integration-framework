f184e41a21ac18c72785b3eeebdc2e4bd0f641ae
jpcerrone <jpcerronex@gmail.com>
2025-08-07 13:30:33 -0300

    Revert "Merge pull request #439 from wazuh/enhancement/30906-remove-manage_agents-binary"
    This reverts commit f8b3ca52635e0042b29414e137f9aa6ec79a80f8, reversing
    changes made to 5f703c5a0eb620669364844382313b51b923ba08.

--- src/wazuh_testing/utils/manage_agents.py
+++ src/wazuh_testing/utils/manage_agents.py
@@ -3,10 +3,13 @@ Copyright (C) 2015-2023, Wazuh Inc.
 Created by Wazuh, Inc. <info@wazuh.com>.
 This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
 """
-from typing import List
+import os
+import subprocess
 import requests
+from typing import List
 
 from wazuh_testing.constants.api import AGENTS_ROUTE
+from wazuh_testing.constants.paths.binaries import MANAGE_AGENTS_BINARY
 from wazuh_testing.modules.api.utils import login, get_base_url
 from wazuh_testing.utils.db_queries import global_db
 
@@ -22,14 +25,17 @@ def remove_agents(agents_id: List, remove_type: str = 'wazuhdb') -> None:
         ValueError: When the type of removal is not correct.
         RuntimeError: When the agent could not be removed via API.
     """
-    if remove_type not in ('wazuhdb', 'api'):
+    if remove_type not in ('wazuhdb', 'manage_agents', 'api'):
         raise ValueError(f"Invalid type of agent removal: {remove_type}")
 
     if agents_id:
-        if remove_type == 'wazuhdb':
-            for agent_id in agents_id:
+        for agent_id in agents_id:
+            if remove_type == 'manage_agents':
+                subprocess.call([MANAGE_AGENTS_BINARY, "-r", f"{agent_id}"], stdout=open(os.devnull, "w"),
+                                stderr=subprocess.STDOUT)
+            elif remove_type == 'wazuhdb':
                 global_db.delete_agent(agent_id)
-        else:
+        if remove_type == 'api':
             authentication_headers, _ = login()
             url = get_base_url() + AGENTS_ROUTE
             payload = {
