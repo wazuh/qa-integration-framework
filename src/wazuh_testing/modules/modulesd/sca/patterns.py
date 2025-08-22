47df4cb76057ff63f6f685f104892f2ccea85f95
Fabio <fabio.contreras@wazuh.com>
2025-08-21 22:03:04 -0500

    test(test_sca): Updated patterns for more accuracy

--- src/wazuh_testing/modules/modulesd/sca/patterns.py
+++ src/wazuh_testing/modules/modulesd/sca/patterns.py
@@ -4,12 +4,12 @@
 from . import PREFIX
 
 # Callback Messages
-NCB_SCA_ENABLED = fr"{PREFIX}INFO: SCA module enabled."
-NCB_SCA_DISABLED = fr"{PREFIX}INFO: SCA module disabled. Exiting."
-NCB_SCA_STARTING = fr"{PREFIX}INFO: Starting SCA module..."
-NCB_SCA_RUNNING = fr"{PREFIX}INFO: SCA module running."
-NCB_SCA_SCAN_STARTED_REQ = fr"{PREFIX}DEBUG: Starting Policy requirements evaluation for policy *"
-NCB_SCA_SCAN_ENDED_REQ = fr"{PREFIX}DEBUG: Policy requirements evaluation completed for policy *"
-NCB_SCA_SCAN_STARTED_CHECK = fr"{PREFIX}DEBUG: Starting Policy checks evaluation for policy *"
-NCB_SCA_SCAN_RESULT = fr"{PREFIX}DEBUG: Policy check \"(\d+)\" evaluation completed for policy \"(.*)\", result: (.*)."
-NCB_SCA_SCAN_ENDED_CHECK = fr"{PREFIX}DEBUG: Policy checks evaluation completed for policy *"
+SCA_ENABLED = fr"{PREFIX}INFO: SCA module enabled"
+SCA_DISABLED = fr"{PREFIX}INFO: SCA module disabled\. Exiting"
+SCA_STARTING = fr"{PREFIX}INFO: Starting SCA module"
+SCA_RUNNING = fr"{PREFIX}INFO: SCA module running"
+SCA_SCAN_STARTED_REQ = fr"{PREFIX}DEBUG: Starting Policy requirements evaluation for policy \"(.*?)\""
+SCA_SCAN_ENDED_REQ = fr"{PREFIX}DEBUG: Policy requirements evaluation completed for policy \"(.*?)\", result: (Passed|Failed)"
+SCA_SCAN_STARTED_CHECK = fr"{PREFIX}DEBUG: Starting Policy checks evaluation for policy \"(.*?)\""
+SCA_SCAN_RESULT = fr"{PREFIX}DEBUG: Policy check \"(\d+)\" evaluation completed for policy \"(.*?)\", result: (Passed|Failed)"
+SCA_SCAN_ENDED_CHECK = fr"{PREFIX}DEBUG: Policy checks evaluation completed for policy \"(.*?)\""
