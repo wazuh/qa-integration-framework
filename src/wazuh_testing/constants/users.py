b2fe945edf101c6b691d00fd8011629cef2498db
jepalfer <jesus.palomares@wazuh.com>
2026-05-26 11:50:08 +0200

    feat: Assignation of users variables according to whenever the wazuh-manager user/group exists

--- src/wazuh_testing/constants/users.py
+++ src/wazuh_testing/constants/users.py
@@ -3,5 +3,23 @@ Copyright (C) 2015-2023, Wazuh Inc.
 Created by Wazuh, Inc. <info@wazuh.com>.
 This program is free software; you can redistribute it and/or modify it under the terms of GPLv2
 """
-WAZUH_UNIX_USER = 'wazuh'
-WAZUH_UNIX_GROUP = 'wazuh'
+import sys
+
+if sys.platform != 'win32':
+    import pwd
+    import grp
+
+    try:
+        pwd.getpwnam('wazuh-manager')
+        WAZUH_UNIX_USER = 'wazuh-manager'
+    except KeyError:
+        WAZUH_UNIX_USER = 'wazuh'
+
+    try:
+        grp.getgrnam('wazuh-manager')
+        WAZUH_UNIX_GROUP = 'wazuh-manager'
+    except KeyError:
+        WAZUH_UNIX_GROUP = 'wazuh'
+else:
+    WAZUH_UNIX_USER = 'wazuh'
+    WAZUH_UNIX_GROUP = 'wazuh'
